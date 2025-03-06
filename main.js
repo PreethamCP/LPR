5deb759d82917f6aafad6babb69fa0e7577c716e
PreethamCP <deepuchowdhalli@gmail.com>
2025-03-06 19:21:09 +0530

    icons

--- main.js
+++ main.js
@@ -14,4 +14,17 @@ document.addEventListener("DOMContentLoaded", function () {
     menuOpen.style.display = "block";
     menuClose.style.display = "none";
   });
+
+  // File Upload Trigger
+  uploadButton.addEventListener("click", function () {
+    fileInput.click();
+    console.log("upload");
+  });
+
+  // Handle File Selection
+  fileInput.addEventListener("change", function () {
+    if (fileInput.files.length > 0) {
+      console.log("File selected:", fileInput.files[0].name);
+    }
+  });
 });
